ed7be964b6282271cc18873a1ece631ca6b4df06
dzakkk <fpdzak@gmail.com>
2024-12-04 17:23:01 +0700

    fix

--- app/Http/Controllers/TransactionController.php
+++ app/Http/Controllers/TransactionController.php
@@ -19,8 +19,8 @@ class TransactionController extends Controller
         // dd($chart1Data);
         // return view('chart.mainChart', compact('chart2Data'));
         $chart3Data = $this->hoursChart();
-        $chart4Data = $this->cardChart();
-        return view('chart.mainChart', compact('chart2Data', 'chart4Data'));
+        // $chart4Data = $this->cardChart();
+        return view('chart.mainChart', compact('chart2Data'));
     }
 
     public function timeChart()
@@ -105,126 +105,6 @@ class TransactionController extends Controller
         ]);
     }
 
-    function cardChart()
-    {
-
-        $timeTrans = Transaction::selectRaw('
-        DATE(timestamp) as Tanggal,
-        COUNT(*) as TotalTransaksi
-    ')
-            ->groupByRaw('DATE(timestamp)')
-            ->orderBy('Tanggal', 'asc')
-            ->get();
-
-
-        $card = DB::table('transactions')->count();
-        $datas = DB::table('transactions')
-            ->select('response_code', DB::raw('count(*) as total'))
-            ->groupBy('response_code')
-            ->get();
-
-        $transInfo = DB::table('transactions')
-            ->select('type_transaksi', DB::raw('count(*) as totalTran'))
-            ->groupBy('type_transaksi')
-            ->get();
-
-        $datass = DB::table('transactions')
-            ->whereNotIn('response_code', ['200'])
-            ->get();
-
-        $datass1 = DB::table('transactions')
-            ->where('response_code', ['0000'])
-            ->get();
-
-        $datass3 = DB::table('transactions')
-            ->where('response_code', ['GAGAL'])
-            ->get();
-
-        $datass4 = DB::table('transactions')
-            ->where('response_code', ['1002'])
-            ->get();
-
-        $datass5 = DB::table('transactions')
-            ->where('response_code', ['mbb1001'])
-            ->get();
-
-        $datass6 = DB::table('transactions')
-            ->where('response_code', ['WS1002'])
-            ->get();
-
-        $datass7 = DB::table('transactions')
-            ->where('response_code', ['ERROR'])
-            ->get();
-
-        $dataTrans = DB::table('transactions')
-            ->where('type_transaksi', ['DANA'])
-            ->get();
-
-        $dataTrans2 = DB::table('transactions')
-            ->where('type_transaksi', ['shopee'])
-            ->get();
-
-        $dataTrans3 = DB::table('transactions')
-            ->where('type_transaksi', ['balance Inquiry'])
-            ->get();
-
-        $dataTrans4 = DB::table('transactions')
-            ->where('type_transaksi', ['connect websocket'])
-            ->get();
-
-        $dataTrans5 = DB::table('transactions')
-            ->where('type_transaksi', ['QRIS'])
-            ->get();
-
-        $dataTrans6 = DB::table('transactions')
-            ->where('type_transaksi', ['ERRORWSCONNECT'])
-            ->get();
-
-        $infoTrans = $dataTrans->pluck('type_transaksi')->count();
-        $infoTrans2 = $dataTrans2->pluck('type_transaksi')->count();
-        $infoTrans3 = $dataTrans3->pluck('type_transaksi')->count();
-        $infoTrans4 = $dataTrans4->pluck('type_transaksi')->count();
-        $infoTrans5 = $dataTrans5->pluck('type_transaksi')->count();
-        $infoTrans6 = $dataTrans6->pluck('type_transaksi')->count();
-        $infoFailed = $datass1->pluck('response_code')->count();
-        $infoFailed3 = $datass3->pluck('response_code')->count();
-        $infoFailed4 = $datass4->pluck('response_code')->count();
-        $infoFailed5 = $datass5->pluck('response_code')->count();
-        $infoFailed6 = $datass6->pluck('response_code')->count();
-        $infoFailed7 = $datass7->pluck('response_code')->count();
-
-        $card2 = $datass->pluck('type_transaksi')->count();
-
-        $card3 = $datas->pluck('response_code')->count();
-
-        $AllTrans = $transInfo->pluck('type_transaksi')->count();
-
-        $waktu = $timeTrans->count();
-
-
-        return [
-            'timeTrans' => $timeTrans,
-            'waktu' => $waktu,
-            'AllTrans' => $AllTrans,
-            'transInfo' => $transInfo,
-            'card' => $card,
-            'card2' => $card2,
-            'card3' => $card3,
-            'infoTrans' => $infoTrans,
-            'infoTrans2' => $infoTrans2,
-            'infoTrans3' => $infoTrans3,
-            'infoTrans4' => $infoTrans4,
-            'infoTrans5' => $infoTrans5,
-            'infoTrans6' => $infoTrans6,
-            'infoFailed' => $infoFailed,
-            'infoFailed' => $infoFailed,
-            'infoFailed3' => $infoFailed3,
-            'infoFailed4' => $infoFailed4,
-            'infoFailed5' => $infoFailed5,
-            'infoFailed6' => $infoFailed6,
-            'infoFailed7' => $infoFailed7,
-        ];
-    }
 
 
     public function components()
